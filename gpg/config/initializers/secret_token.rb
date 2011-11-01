16a9c2fdeecab04842c7c082da828ca1faa426b3
Francesco Pellegrini <kranz@ubukranz.localdomain>
2011-11-01 17:35:46 +0100

    Partenza progetto gpg

---
+++ gpg/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gpg::Application.config.secret_token = '4a025f01d548a4d3024b02a7173a2945a59425e8190fdf2f50801c3f0ca783249483fdc7131bb41c6d3a640dcb2ebc98fac77adbcd9b8c04b44f987473e10bc3'
